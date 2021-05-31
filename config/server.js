35edcefde1f8270235547c94270ab06d0521eb37
visheshdvn <vishesh.dh1@gmail.com>
2021-05-31 22:38:36 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b0e551f84057d1e6317126f240cef531'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ebcc9ffe-b94a-438e-b7e1-38ce8929e74a'
+};
